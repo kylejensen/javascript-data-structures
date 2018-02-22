a21a6533823c36b9fe78cd884b637fa23f621d90
Kyle Jensen <kyjen02@gmail.com>
2018-02-21 19:08:24 -0700

    Inital commit

---
+++ linked_list_notail.js
@@ -0,0 +1,110 @@
+function Node (data) {
+    this._data = data;
+    this.next = null;
+}
+
+function SinglyLinkedList() {
+    this._size = 0;
+    this._head = null;
+}
+
+SinglyLinkedList.prototype.size = function () {
+    // O(1)
+    return this._size;
+};
+
+SinglyLinkedList.prototype.empty = function () {
+    // O(1)
+    return this._size === 0;
+};
+
+SinglyLinkedList.prototype.valueAt = function (index) {
+    // O(N)
+    let item = this._head,
+        count = 0;
+    
+    while (item.next !== null && count !== index) {
+        item = item.next; 
+        count += 1;
+    }
+    if (count === index && item !== null) {
+        return item._data;
+    }
+
+    return null;
+};
+
+SinglyLinkedList.prototype.pushFront = function (data) {
+    // O(1)
+    let node = new Node(data);
+    if (this._size === 0) {
+        this._head = node;
+    } else {
+        node.next = this._head;
+        this._head = node;
+    }
+    this._size += 1;
+};
+
+SinglyLinkedList.prototype.popFront = function () {
+    // O(1)
+    if (this._size > 0) {
+        let deleted = this._head;
+        this._head = this._head.next;
+        this._size -= 1;
+        return deleted._data;
+    }
+    return "Underflow!";
+};
+
+SinglyLinkedList.prototype.pushBack = function (data) {
+    // O(N)
+    let node = new Node(data);
+    if (this._head === null) {
+        this._head = node;
+    } else {
+        let item = this._head;
+        while (item.next !== null) {
+            item = item.next;
+        }
+        item.next = node;
+        this._size += 1;
+    }
+};
+
+SinglyLinkedList.prototype.popBack = function () {
+    // O(N)
+    if (this._size > 0) {
+        let item = this._head;
+        while (item.next.next !== null) {
+            item = item.next;
+        }
+        let deleted = item.next;
+        delete item.next;
+        item.next = null;
+        this._size -= 1;
+        return deleted._data;
+    }
+    
+    return "Underflow!";
+};
+
+SinglyLinkedList.prototype.front = function () {
+    // O(1)
+    if (this._size > 0) {
+        return this._head._data;
+    }
+    return "No items in list";
+};
+
+SinglyLinkedList.prototype.back = function () {
+    // O(N)
+    if (this._size > 0) {
+        let item = this._head;
+        while (item.next !== null) {
+            item = item.next;
+        }
+        return item._data;
+    }
+    return "No items in list";
+};
