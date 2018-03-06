ba6a9afc7c0ec74e5adb593992c0335a5b3feb57
Kyle Jensen <kyjen02@gmail.com>
2018-03-05 18:02:03 -0700

    Updates linked list with tail

--- linked_list_tail.js
+++ linked_list_tail.js
@@ -104,3 +104,122 @@ SinglyLinkedList.prototype.back = function () {
     }
     return "No items in list";
 };
+
+SinglyLinkedList.prototype.insert = function (index, value) {
+    // O(N)
+    var count = 0,
+        item = this._head,
+        node = new Node(value);
+
+    if (index === 0) {
+        node.next = this._head
+        this._head = node;
+        this._size++;
+        return;
+    } else {
+        while (item.next !== null && count < index - 1) {
+            item = item.next;
+            count++;
+        }
+    }
+    
+    if (count === index - 1) {
+        node.next = item.next;
+        item.next = node;
+        this._size++;
+    } else {
+        return "Index out of range! Please choose a value between 0 and " + this._size;
+    }
+};
+
+SinglyLinkedList.prototype.erase = function (index) {
+    // O(N)
+    var count = 0,
+        item = this._head;
+
+    if (index === 0) {
+        this._head = item.next;
+        this._size--;
+        return;
+    } else {
+        while (item.next !== null && count < index - 1) {
+            item = item.next;
+            count++;
+        }
+    }
+    
+    if (count === index - 1) {
+        item.next = item.next.next;
+        if (item.next === null) {
+            this._tail = item;
+        }
+        this._size--;
+    } else {
+        return "Index out of range! Please choose a value between 0 and " + this._size;
+    }
+};
+
+SinglyLinkedList.prototype.removeValue = function (value) {
+    // O(N)
+    var item = this._head;
+    if (item._data === value) {
+        this._head = item.next;
+        this._size--;
+    } else {
+        while (item.next !== null && item.next._data !== value) {
+            item = item.next;
+        }
+    }
+    if (item.next === null) {
+        return "Value not in list";
+    } else {
+        item.next = item.next.next;
+        this._size--;
+    }
+};
+
+SinglyLinkedList.prototype.valueNFromEnd = function (n) {
+    // O(N)
+    var listLength = 0,
+        item = this._head,
+        travelDistance;
+
+    while (item.next) {
+        item = item.next;
+        listLength++;
+    }
+
+    if (n > listLength) {
+        return "Out of bounds";
+    } else if (n === 0) {
+        return item._data;
+    }
+
+    travelDistance = listLength - n;
+    item = this._head;
+
+    while (travelDistance !== 0) {
+        item = item.next;
+        travelDistance--;
+    }
+
+    return item._data;
+};
+
+SinglyLinkedList.prototype.reverse = function () { 
+    // O(N)
+    var currentNode = this._head,
+        previousNode = null,
+        nextNode = null;
+
+    this._tail = currentNode;
+
+    while (currentNode) {
+        nextNode = currentNode.next;
+        currentNode.next = previousNode;
+        previousNode = currentNode;
+        currentNode = nextNode;
+    }
+
+    this._head = previousNode;
+};
