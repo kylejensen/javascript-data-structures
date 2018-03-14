2fb76c6bf316c9e355dbe968a6b9b4aafb56ac8f
Kyle Jensen <kyjen02@gmail.com>
2018-03-14 17:05:33 -0600

    Updates ll no tail to es6

--- linked_list_notail.js
+++ linked_list_notail.js
@@ -1,224 +1,228 @@
-function Node (data) {
-    this._data = data;
-    this.next = null;
+class Node {
+    constructor(data, next = null) {
+        this._data = data;
+        this.next = next;
+    }
 }
 
-function SinglyLinkedList() {
-    this._size = 0;
-    this._head = null;
-}
+class SinglyLinkedList {
+    constructor() {
+        this._size = 0;
+        this._head = null;
+    }
 
-SinglyLinkedList.prototype.size = function () {
-    // O(1)
-    return this._size;
-};
+    size() {
+        // O(1)
+        return this._size;
+    }
 
-SinglyLinkedList.prototype.empty = function () {
-    // O(1)
-    return this._size === 0;
-};
+    empty() {
+        // O(1)
+        return this._size === 0;
+    }
 
-SinglyLinkedList.prototype.valueAt = function (index) {
-    // O(N)
-    let item = this._head,
+    valueAt(index) {
+        // O(N)
+        let item = this._head,
         count = 0;
-    
-    while (item.next !== null && count !== index) {
-        item = item.next; 
-        count += 1;
-    }
-    if (count === index && item !== null) {
-        return item._data;
+
+        while (item.next !== null && count !== index) {
+            item = item.next; 
+            count += 1;
+        }
+        if (count === index && item !== null) {
+            return item._data;
+        }
+
+        return null;
     }
 
-    return null;
-};
-
-SinglyLinkedList.prototype.pushFront = function (data) {
-    // O(1)
-    let node = new Node(data);
-    if (this._size === 0) {
-        this._head = node;
-    } else {
-        node.next = this._head;
-        this._head = node;
+    pushFront(data) {
+        // O(1)
+        let node = new Node(data);
+        if (this._size === 0) {
+            this._head = node;
+        } else {
+            node.next = this._head;
+            this._head = node;
+        }
+        this._size += 1;
     }
-    this._size += 1;
-};
 
-SinglyLinkedList.prototype.popFront = function () {
-    // O(1)
-    if (this._size > 0) {
-        let deleted = this._head;
-        this._head = this._head.next;
-        this._size -= 1;
-        return deleted._data;
+    popFront() {
+        // O(1)
+        if (this._size > 0) {
+            let deleted = this._head;
+            this._head = this._head.next;
+            this._size -= 1;
+            return deleted._data;
+        }
+        return "Underflow!";
     }
-    return "Underflow!";
-};
 
-SinglyLinkedList.prototype.pushBack = function (data) {
-    // O(N)
-    let node = new Node(data);
-    if (this._head === null) {
-        this._head = node;
-    } else {
+    pushBack(data) {
+        // O(N)
+        let node = new Node(data);
+        if (this._head === null) {
+            this._head = node;
+        } else {
+            let item = this._head;
+            while (item.next !== null) {
+                item = item.next;
+            }
+            item.next = node;
+        }
+        this._size += 1;
+    }
+
+    popBack() {
+        // O(N)
+        if (this._size > 0) {
+            let item = this._head;
+            while (item.next.next !== null) {
+                item = item.next;
+            }
+            let deleted = item.next;
+            delete item.next;
+            item.next = null;
+            this._size -= 1;
+            return deleted._data;
+        }
+        
+        return "Underflow!";
+    }
+
+    front() {
+        // O(1)
+        if (this._size > 0) {
+            return this._head._data;
+        }
+        return "No items in list";
+    }
+
+    back() {
+        // O(N)
+        if (this._size > 0) {
+            let item = this._head;
+            while (item.next !== null) {
+                item = item.next;
+            }
+            return item._data;
+        }
+        return "No items in list";
+    }
+
+    insert(index, value) {
+        // O(N)
+        let count = 0,
+            item = this._head,
+            node = new Node(value);
+
+        if (index === 0) {
+            node.next = this._head
+            this._head = node;
+            this._size++;
+            return;
+        } else {
+            while (item.next !== null && count < index - 1) {
+                item = item.next;
+                count++;
+            }
+        }
+
+        if (count === index - 1) {
+            node.next = item.next;
+            item.next = node;
+            this._size++;
+        } else {
+            return "Index out of range! Please choose a value between 0 and " + this._size;
+        }
+    }
+
+    erase(index) {
+        // O(N)
+        let count = 0,
+            item = this._head;
+
+        if (index === 0) {
+            this._head = item.next;
+            this._size--;
+            return;
+        } else {
+            while (item.next !== null && count < index - 1) {
+                item = item.next;
+                count++;
+            }
+        }
+
+        if (count === index - 1) {
+            item.next = item.next.next;
+            this._size--;
+        } else {
+            return "Index out of range! Please choose a value between 0 and " + this._size;
+        }
+    }
+
+    removeValue(value) {
+        // O(N)
         let item = this._head;
-        while (item.next !== null) {
-            item = item.next;
+        if (item._data === value) {
+            this._head = item.next;
+            this._size--;
+        } else {
+            while (item.next !== null && item.next._data !== value) {
+                item = item.next;
+            }
         }
-        item.next = node;
-    }
-    this._size += 1;
-};
-
-SinglyLinkedList.prototype.popBack = function () {
-    // O(N)
-    if (this._size > 0) {
-        let item = this._head;
-        while (item.next.next !== null) {
-            item = item.next;
-        }
-        let deleted = item.next;
-        delete item.next;
-        item.next = null;
-        this._size -= 1;
-        return deleted._data;
-    }
-    
-    return "Underflow!";
-};
-
-SinglyLinkedList.prototype.front = function () {
-    // O(1)
-    if (this._size > 0) {
-        return this._head._data;
-    }
-    return "No items in list";
-};
-
-SinglyLinkedList.prototype.back = function () {
-    // O(N)
-    if (this._size > 0) {
-        let item = this._head;
-        while (item.next !== null) {
-            item = item.next;
-        }
-        return item._data;
-    }
-    return "No items in list";
-};
-
-SinglyLinkedList.prototype.insert = function (index, value) {
-    // O(N)
-    var count = 0,
-        item = this._head,
-        node = new Node(value);
-
-    if (index === 0) {
-        node.next = this._head
-        this._head = node;
-        this._size++;
-        return;
-    } else {
-        while (item.next !== null && count < index - 1) {
-            item = item.next;
-            count++;
+        if (item.next === null) {
+            return "Value not in list";
+        } else {
+            item.next = item.next.next;
+            this._size--;
         }
     }
-    
-    if (count === index - 1) {
-        node.next = item.next;
-        item.next = node;
-        this._size++;
-    } else {
-        return "Index out of range! Please choose a value between 0 and " + this._size;
-    }
-};
 
-SinglyLinkedList.prototype.erase = function (index) {
-    // O(N)
-    var count = 0,
+    valueNFromEnd(n) {
+        // O(N)
+        let listLength = 0,
+            item = this._head,
+            travelDistance;
+
+        while (item.next) {
+            item = item.next;
+            listLength++;
+        }
+
+        if (n > listLength) {
+            return "Out of bounds";
+        } else if (n === 0) {
+            return item._data;
+        }
+
+        travelDistance = listLength - n;
         item = this._head;
 
-    if (index === 0) {
-        this._head = item.next;
-        this._size--;
-        return;
-    } else {
-        while (item.next !== null && count < index - 1) {
+        while (travelDistance !== 0) {
             item = item.next;
-            count++;
+            travelDistance--;
         }
-    }
-    
-    if (count === index - 1) {
-        item.next = item.next.next;
-        this._size--;
-    } else {
-        return "Index out of range! Please choose a value between 0 and " + this._size;
-    }
-};
 
-SinglyLinkedList.prototype.removeValue = function (value) {
-    // O(N)
-    var item = this._head;
-    if (item._data === value) {
-        this._head = item.next;
-        this._size--;
-    } else {
-        while (item.next !== null && item.next._data !== value) {
-            item = item.next;
-        }
-    }
-    if (item.next === null) {
-        return "Value not in list";
-    } else {
-        item.next = item.next.next;
-        this._size--;
-    }
-};
-
-SinglyLinkedList.prototype.valueNFromEnd = function (n) {
-    // O(N)
-    var listLength = 0,
-        item = this._head,
-        travelDistance;
-
-    while (item.next) {
-        item = item.next;
-        listLength++;
-    }
-
-    if (n > listLength) {
-        return "Out of bounds";
-    } else if (n === 0) {
         return item._data;
     }
 
-    travelDistance = listLength - n;
-    item = this._head;
+    reverse() {
+        // O(N)
+        let currentNode = this._head,
+            previousNode = null,
+            nextNode = null;
 
-    while (travelDistance !== 0) {
-        item = item.next;
-        travelDistance--;
+        while (currentNode) {
+            nextNode = currentNode.next;
+            currentNode.next = previousNode;
+            previousNode = currentNode;
+            currentNode = nextNode;
+        }
+
+        this._head = previousNode;
     }
-
-    return item._data;
-};
-
-SinglyLinkedList.prototype.reverse = function () { 
-    // O(N)
-    var currentNode = this._head,
-        previousNode = null,
-        nextNode = null;
-
-    while (currentNode) {
-        nextNode = currentNode.next;
-        currentNode.next = previousNode;
-        previousNode = currentNode;
-        currentNode = nextNode;
-    }
-
-    this._head = previousNode;
-};
+}
