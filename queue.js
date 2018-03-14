5a3d483514b39b58ac0ef92aed8fc2e3ad67a0d4
Kyle Jensen <kyjen02@gmail.com>
2018-03-14 16:48:23 -0600

    Updates queue to es6

--- queue.js
+++ queue.js
@@ -1,40 +1,44 @@
-var Queue = function () {
-    this._head = null;
-    this._tail = null;
-    this._size = 0;
-};
-
-var Node = function (data) {
-    this._data = data;
-    this.next = null;
-};
-
-Queue.prototype.empty = function () {
-    return this._size === 0;
-};
-
-Queue.prototype.enqueue = function (value) {
-    var node = new Node(value);
-    if (this._head === null) {
-        this._head = node;
-        this._tail = node;
-    } else {
-        this._tail.next = node;
-        this._tail = node;
+class Node {
+    constructor(data, next = null) {
+        this._data = data;
+        this.next = next;
     }
-    this._size++;
-};
+}
 
-Queue.prototype.dequeue = function () {
-    if (this._size > 0) {
-        var item = this._head;
-        this._head = this._head.next;
-        if (this._size === 1) {
-            this._tail = null;
+class Queue {
+    constructor() {
+        this._head = null;
+        this._tail = null;
+        this._size = 0;
+    }
+
+    empty() {
+        return this._size === 0;
+    }
+
+    enqueue(data) {
+        let node = new Node(data);
+        if (this._head === null) {
+            this._head = node;
+            this._tail = node;
+        } else {
+            this._tail.next = node;
+            this._tail = node;
         }
-        this._size--;
-        return item._data;
-    } else {
-        return "Underflow!";
+        this._size += 1;
     }
-};
+
+    dequeue(data) {
+        if (this._size > 0) {
+            let item = this._head;
+            this._head = this._head.next;
+            if (this._size === 1) {
+                this._tail = null;
+            }
+            this._size--;
+            return item._data;
+        } else {
+            return "Underflow!";
+        }
+    }
+}
