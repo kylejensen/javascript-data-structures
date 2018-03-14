9c6df52689adcbd87567c22befbae267dda4eef7
Kyle Jensen <kyjen02@gmail.com>
2018-03-14 16:04:21 -0600

    Adds remove method to BST

--- binarysearchtree.js
+++ binarysearchtree.js
@@ -198,4 +198,40 @@ class BST {
             return null;
         }
     }
+
+    remove(data) {
+        let removeNode = (node, data) => {
+            if (node === null) {
+                return null;
+            }
+
+            if (data === node.data) {
+                if (node.left === null && node.right === null) {
+                    return null;
+                }
+                if (node.left === null) {
+                    return node.right;
+                }
+                if (node.right === null) {
+                    return node.left;
+                }
+
+                let tempNode = node.right;
+                while (tempNode.left !== null) {
+                    tempNode = tempNode.left;
+                }
+                node.data = tempNode.data;
+                node.right = removeNode(node.right, tempNode.data);
+                return node;
+            } else if (data < node.data) {
+                node.left = removeNode(node.left, data);
+                return node;
+            } else if (data > node.data) {
+                node.right = removeNode(node.right, data);
+                return node;
+            }
+        };
+
+        this.root = removeNode(this.root, data);
+    }
 }
